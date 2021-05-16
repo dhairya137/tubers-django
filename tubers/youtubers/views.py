8c25f4cae163e4703d187d89101a7e6f0fc13d95
Dhairya <dhairya00798@gmail.com>
2021-05-16 19:55:32 +0530

    single youtuber component done

--- tubers/youtubers/views.py
+++ tubers/youtubers/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Youtuber
 
 # Create your views here.
 def youtubers(request):
-  pass
+  tubers = Youtuber.objects.order_by('-created_date')
+  data = {
+    'tubers': tubers
+  }
+  return render(request, 'youtubers/youtubers.html', data)
+  
 
 def youtubers_detail(request, id):
-  pass
+  tuber = get_object_or_404(Youtuber, pk=id)
+  data = {
+    'tuber': tuber
+  }
+  return render(request, 'youtubers/youtuber_detail.html', data)
 
 def search(request):
   pass
