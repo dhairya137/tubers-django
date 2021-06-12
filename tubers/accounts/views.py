efae79ae1b7d7d2381e9d6394206f21ad898128a
Dhairya <dhairya00798@gmail.com>
2021-06-12 22:30:50 +0530

    user registration done

--- tubers/accounts/views.py
+++ tubers/accounts/views.py
@@ -1,10 +1,39 @@
 from django.shortcuts import render, redirect 
 from django.contrib.auth import logout
+from django.contrib.auth.models import User
+from django.contrib import messages, auth
 
 def login(request):
   return render(request, 'accounts/login.html')
 
 def register(request):
+  if request.method == 'POST':
+    firstname = request.POST['firstname']
+    lastname = request.POST['lastname']
+    username = request.POST['username']
+    email = request.POST['email']
+    password = request.POST['password']
+    confirm_password = request.POST['confirm_password']
+
+    if password == confirm_password:
+      if User.objects.filter(username=username).exists():   #? first username is database field and second username is our var. name
+        messages.error(request, "Username exists")
+        return redirect('register')
+      else:
+        if User.objects.filter(email=email).exists():
+          messages.error(request, "Email exists")
+          return redirect('register')
+        else:
+          user = User.objects.create_user(first_name=firstname, last_name=lastname, username=username, email=email, password=password)
+          user.save()
+          messages.success(request, 'User created hurrey!!')
+          return redirect('login')
+    else:
+      messages.error(request, 'Password does not match')
+      return redirect('register')
+
+    # 
+
   return render(request, 'accounts/register.html')
 
 def logout_user(request):
