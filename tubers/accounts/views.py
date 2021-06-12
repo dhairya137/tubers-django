ee7d3cd02d761ad4c98861b9c4549550a3afffbd
Dhairya <dhairya00798@gmail.com>
2021-06-12 22:47:42 +0530

    dashboard route secured

--- tubers/accounts/views.py
+++ tubers/accounts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 from django.contrib.auth.models import User
 from django.contrib import messages, auth
+from django.contrib.auth.decorators import login_required
+
 
 def login(request):
   if request.method == 'POST':
@@ -54,5 +56,6 @@ def logout_user(request):
   logout(request)
   return redirect('home')
 
+@login_required(login_url='login')
 def dashboard(request):
   return render(request, 'accounts/dashboard.html')
